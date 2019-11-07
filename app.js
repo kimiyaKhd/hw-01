77bb2bc41054914f9c3c77327ab814889a5ebc26
kimiyaKhd <55557196+kimiyaKhd@users.noreply.github.com>
2019-11-07 08:22:38 +0330

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+var hotel = { color: "red", floor: 30, height: 600, owner: "kimiya", star: 5, state: true };
+var value = "true";
+var property_name = prompt("enter the name of property to see value");
+var answer = prompt("the value of " + property_name + " is " + hotel[property_name] + "  do you want to update this?(yes or no)");
+(answer == "yes" && typeof hotel[property_name] == "number") ? hotel[property_name] = Number(prompt("enter the new value")): (answer == "yes" && typeof hotel[property_name] == "string") ? hotel[property_name] = prompt("enter the new value") : (answer == "yes" && typeof hotel[property_name] == "boolean") ? hotel[property_name] = (value == prompt("enter the new value")) : false;
+
+console.log(hotel);
